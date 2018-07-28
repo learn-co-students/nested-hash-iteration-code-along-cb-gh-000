545082eee3fca314406456e510efb2db3eeb8da3
Koy Saeteurn <kosatun@aol.com>
2018-07-28 06:23:33 +0000

    Done.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,13 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+  contacts.each do |person, details_hash|
+    
+    details_hash.each do |attribute, value|
+      
+      if attribute == :favorite_ice_cream_flavors
+        value.delete_if {|ice_cream| ice_cream == "strawberry"}
+      end  
+    end
+  end    
 end
